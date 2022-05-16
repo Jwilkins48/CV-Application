90151f0237c94f77a7991c5daec539cd82b96180
Jenna <jenna48071@gmail.com>
2022-05-16 16:41:25 -0500

    created simple header

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Header extends Component {
+    render(){
+        return(
+            <nav>
+                <div className="headerContainer">
+                    <h1 className="headerTitle">CV Application</h1>
+
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header
